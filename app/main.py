a7b18ea0eb35dd48be2edbc9202158d6d3f499a2
Botvynko95 <botvynko95@gmail.com>
2025-11-29 11:12:51 +0100

    'Solution'

--- app/main.py
+++ app/main.py
@@ -2,25 +2,31 @@ from __future__ import annotations
 
 
 class Animal:
-    alive = []
+    alive: list["Animal"] = []
 
     def __init__(
-            self: "Animal",
-            name: str,
-            health: int = 100,
+        self: "Animal",
+        name: str,
+        health: int = 100,
     ) -> None:
         self.name = name
         self._health = health
         self.hidden = False
         Animal.alive.append(self)
 
-    def __repr__(self: "Animal") -> str:
-        return (f"{{Name: {self.name}, "
-                f"Health: {self.health}, "
-                f"Hidden: {self.hidden}}}")
+    def __repr__(
+        self: "Animal",
+    ) -> str:
+        return (
+            f"{{Name: {self.name}, "
+            f"Health: {self.health}, "
+            f"Hidden: {self.hidden}}}"
+        )
 
     @property
-    def health(self: "Animal") -> int:
+    def health(
+        self: "Animal",
+    ) -> int:
         return self._health
 
     @health.setter
@@ -29,22 +35,27 @@ class Animal:
         value: int,
     ) -> None:
         self._health = max(0, value)
+
         if self._health == 0 and self in Animal.alive:
             Animal.alive.remove(self)
 
 
 class Herbivore(Animal):
-    def hide(self: "Herbivore") -> None:
+    def hide(
+        self: "Herbivore",
+    ) -> None:
         self.hidden = not self.hidden
 
 
 class Carnivore(Animal):
     def bite(
-            self: "Carnivore",
-            other: Herbivore,
+        self: "Carnivore",
+        other: Animal,
     ) -> None:
         if not isinstance(other, Herbivore):
             return
+
         if other.hidden:
             return
+
         other.health = other.health - 50
