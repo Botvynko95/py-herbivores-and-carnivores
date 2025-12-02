d0ca58aad7e12b912d8327681c8cf67369d9dded
Botvynko95 <botvynko95@gmail.com>
2025-12-02 16:44:06 +0100

    'Solution'

--- app/main.py
+++ app/main.py
@@ -5,57 +5,38 @@ class Animal:
     alive: list["Animal"] = []
 
     def __init__(
-        self: "Animal",
+        self,
         name: str,
         health: int = 100,
     ) -> None:
         self.name = name
-        self._health = health
+        self.health = health
         self.hidden = False
         Animal.alive.append(self)
 
-    def __repr__(
-        self: "Animal",
-    ) -> str:
+    def die(self) -> None:
+        if self in Animal.alive:
+            Animal.alive.remove(self)
+
+    def __repr__(self) -> str:
         return (
             f"{{Name: {self.name}, "
             f"Health: {self.health}, "
             f"Hidden: {self.hidden}}}"
         )
 
-    @property
-    def health(
-        self: "Animal",
-    ) -> int:
-        return self._health
-
-    @health.setter
-    def health(
-        self: "Animal",
-        value: int,
-    ) -> None:
-        self._health = max(0, value)
-
-        if self._health == 0 and self in Animal.alive:
-            Animal.alive.remove(self)
-
 
 class Herbivore(Animal):
-    def hide(
-        self: "Herbivore",
-    ) -> None:
+    def hide(self) -> None:
         self.hidden = not self.hidden
 
 
 class Carnivore(Animal):
-    def bite(
-        self: "Carnivore",
-        other: Animal,
-    ) -> None:
-        if not isinstance(other, Herbivore):
-            return
+    def bite(self, other: Animal) -> None:
+        if (isinstance(other, Herbivore)
+                and not other.hidden and other.health > 0):
+            other.health -= 50
 
-        if other.hidden:
-            return
-
-        other.health = other.health - 50
+        if other.health <= 0:
+            other.health = 0
+            other.die()
